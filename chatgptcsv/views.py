fb3c520d920b0c392ca4324550359320948673fa
Jose <jose.fcr13@gmail.com>
2023-10-11 10:54:09 -0500

    fixed bugs and updated chatgpt engine

--- chatgptcsv/views.py
+++ chatgptcsv/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .forms import CSVUploadForm
 from .models import UploadedCSV
 import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def upload_csv(request):
     if request.method == 'POST':
@@ -17,14 +21,15 @@ def upload_csv(request):
 def process_csv_with_chatgpt(csv_file_path):
     # Add code to process the CSV file using ChatGPT
     # For example, using OpenAI's GPT-3 API
-    openai.api_key = 'your-own-api-key' # You should generate api key to test this project
+    openai.api_key = os.getenv('OPENAI_API_KEY') # You should generate api key to test this project
     with open(csv_file_path, 'r') as file:
-        content = file.read()
+        prompt = f"analize this content and give me your opinion {file.read()}"
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=content,
-        max_tokens=100,
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=250,
         n=1,
-        stop=None
+        stop=None,
+        temperature=0.5,
     )
     return response.choices[0].text
